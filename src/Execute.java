b9266daa47329b681054129e550826bd7c308887
adrianbadarau <adi.badarau@yahoo.com>
2015-02-21 01:03:08 +0200

    made the insert statement

--- src/Execute.java
+++ src/Execute.java
@@ -1,6 +1,10 @@
 import controllers.*;
+import server.DataBaseConnection;
 import server.MyServer;
 import views.*;
+
+import java.sql.SQLException;
+import java.util.HashMap;
 
 /**
  * Created by adrian on 18.02.2015.
@@ -14,5 +18,17 @@ public class Execute {
         MyServer server = new MyServer();
         Thread thread = new Thread(server);
         thread.start();
+        DataBaseConnection con = new DataBaseConnection();
+        Thread dbThread = new Thread(con);
+        dbThread.run();
+        HashMap<String,String> doSql = new HashMap<>();
+        doSql.put("CONTACT_ID","33");
+        doSql.put("TITLE","'TESTSQL'");
+        doSql.put("BODY","'LALALALALAALALALA'");
+        try {
+            System.out.println(con.insert("APP.SENT_MESSAGES",doSql));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/server/DataBaseConnection.java
+++ src/server/DataBaseConnection.java
@@ -3,19 +3,48 @@ package server;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by adib on 2/20/15.
  */
 public class DataBaseConnection implements Runnable{
-    public Connection dbCon;
+    public static Connection dbCon;
     @Override
     public void run() {
         System.out.print("New thread");
-        try  {
-            dbCon = DriverManager.getConnection("jdbc:derby:LocalDB");
-        }catch (SQLException e) {
-            e.printStackTrace();
+//        try  {
+//            dbCon = DriverManager.getConnection("jdbc:derby:LocalDB");
+//        }catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//        try(Connection cn = DriverManager.getConnection("jdbc:derby:LocalDB;create=true")) {
+//            dbCon = cn;
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+    }
+
+    public boolean insert(String table, HashMap<String,String> values) throws SQLException {
+        dbCon  = DriverManager.getConnection("jdbc:derby:LocalDB;create=true");
+        StringBuilder sql = new StringBuilder();
+        StringBuilder vars = new StringBuilder();
+        sql.append("INSERT INTO "+table+" (");
+//        vars.append("(");
+        for(Map.Entry<String, String> value :values.entrySet()){
+            sql.append(value.getKey()+",");
+            vars.append(value.getValue()+",");
         }
+        sql.deleteCharAt(sql.length()-1);
+        vars.deleteCharAt(vars.length()-1);
+        sql.append(") VALUES (" + vars.toString() + ")");
+        Statement query = dbCon.createStatement();
+        boolean rez = query.execute(sql.toString());
+        dbCon.close();
+        return rez;
+
     }
 }
